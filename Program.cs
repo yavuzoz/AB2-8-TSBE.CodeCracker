b6516c96a5b8f4df423754e76a8a1f880bb8935b
Yavuz Özbay <ozbayavz@gmail.com>
2023-09-12 23:39:21 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Tsbe.CodeCracker;
+
+namespace AB2_8_TSBE.CodeCracker
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Bitte Passwort eingeben:");
+			string password = Console.ReadLine();
+
+			string targetHash = MD5Hash.GeneratePassword(password);
+
+			Console.WriteLine($"Ihr Passwort als Hashcode ist: {targetHash}");
+			Console.WriteLine("Bitte max Länge eingeben: ");
+			int maxLength = int.Parse(Console.ReadLine());
+
+			char[] allowedCharacters = "abcdefghijklmnopqrstuvwxyz0123456789".ToCharArray(); 
+
+			string crackedPassword = MD5Hash.CrackPassword(targetHash, maxLength, allowedCharacters);
+
+			if (crackedPassword != null)
+			{
+				Console.WriteLine($"Cracked Password: {crackedPassword}");
+			}
+			else
+			{
+				Console.WriteLine("Password not cracked.");
+			}
+		}
+	}
+}
